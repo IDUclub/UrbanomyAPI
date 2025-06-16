473159fda6d5957a6540fef8b3914f2143b0a973
Voronapxl <trololo695@gmail.com>
2025-06-16 13:08:45 +0300

    fix(dto): added correct argument name

--- app/urbanomy_api/dto/investment_attractivness_dto.py
+++ app/urbanomy_api/dto/investment_attractivness_dto.py
@@ -5,8 +5,8 @@ from app.urbanomy_api.dto.benchmarks_dto import BenchmarksDto
 
 
 class InvestmentAttractivenessRequestDto(BaseModel):
-    scenario_id: int = Field(..., example=198, description="Scenario id")
-    as_geojson: bool = Field(..., example=False, description="Which format to return")
+    scenario_id: int = Field(..., examples=[198], description="Scenario id")
+    as_geojson: bool = Field(..., examples=[False], description="Which format to return")
     benchmarks: BenchmarksDto = Body(
         ...,
         description="Benchmark parameters for each functional zone category"
